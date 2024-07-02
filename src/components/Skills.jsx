ce90941a97bd2aa051ce6f6d7650e7e3d51de93e
SumanSaha <8759127658itisaha@gmail.com>
2024-07-02 18:51:44 +0530

    "Updated Skills component to import icons from local files instead of using hardcoded URLs."

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,13 @@
 import React from 'react'
+import htmlIcon from '../../public/html-icon.jpg'
+import cssIcon from '../../public/css-icon.jpg'
+import jsIcon from '../../public/js-icon.jpg'
+import reactIcon from '../../public/react-icon.jpg'
+import tailwindIcon from '../../public/tailwind-icon.png'
+import nodeIcon from '../../public/node-icon.png'
+import expressIcon from '../../public/express-icon.png'
+import mongodbIcon from '../../public/mongo-icon.jpg'
+import githubIcon from '../../public/github-icon.png'
 
 const Skills = () => {
   return (
@@ -11,64 +20,65 @@ const Skills = () => {
         </div>
         
         <div className="graph-container w-full flex flex-col gap-4 grid md:grid-cols-3 grid-cols-2" >
+
             <div className="html hover:scale-105 duration-200 cursor-pointer">
-                <span className="flex py-2 gap-2 text-sm font-medium"><img src="/public/html-icon.jpg" alt="" className="w-[24px]" />HTML</span>
+                <span className="flex py-2 gap-2 text-sm font-medium"><img src={htmlIcon} alt="" className="w-[24px]" />HTML</span>
                 <div className="w-[80%] h-2 bg-slate-200 rounded-full">
                     <div className="bg-[#83bdf5] w-[75%] h-2 rounded-full"></div>
                 </div>
             </div>
 
             <div className="css hover:scale-105 duration-200 cursor-pointer">
-            <span className="flex py-2 gap-2 text-sm font-medium"><img src="/public/css-icon.jpg" alt="" className="w-[24px]" />CSS</span>
+            <span className="flex py-2 gap-2 text-sm font-medium"><img src={cssIcon} alt="" className="w-[24px]" />CSS</span>
             <div className="w-[80%] h-2 bg-slate-200 rounded-full">
                     <div className="bg-[#83bdf5] w-[78%] h-2 rounded-full"></div>
                 </div>
             </div>
 
             <div className="js hover:scale-105 duration-200 cursor-pointer">
-            <span className="flex py-2 gap-2 text-sm font-medium"><img src="/public/js-icon.jpg" alt="" className="w-[24px]" />JAVASCRIPT</span>
+            <span className="flex py-2 gap-2 text-sm font-medium"><img src={jsIcon} alt="" className="w-[24px]" />JAVASCRIPT</span>
             <div className="w-[80%] h-2 bg-slate-200 rounded-full">
                     <div className="bg-[#83bdf5] w-[60%] h-2 rounded-full"></div>
                 </div>
             </div>
 
             <div className="react hover:scale-105 duration-200 cursor-pointer">
-            <span className="flex py-2 gap-2 text-sm font-medium"><img src="/public/react-icon.jpg" alt="" className="w-[24px]" />REACT JS</span>
+            <span className="flex py-2 gap-2 text-sm font-medium"><img src={reactIcon} alt="" className="w-[24px]" />REACT JS</span>
             <div className="w-[80%] h-2 bg-slate-200 rounded-full">
                     <div className="bg-[#83bdf5] w-[65%] h-2 rounded-full"></div>
                 </div>
             </div>
 
             <div className="tailwind hover:scale-105 duration-200 cursor-pointer">
-            <span className="flex py-2 gap-2 text-sm font-medium"><img src="/public/tailwind-icon.png" alt="" className="w-[24px]" />TAILWIND CSS</span>
+            <span className="flex py-2 gap-2 text-sm font-medium"><img src={tailwindIcon} alt="" className="w-[24px]" />TAILWIND CSS</span>
             <div className="w-[80%] h-2 bg-slate-200 rounded-full">
                     <div className="bg-[#83bdf5] w-[70%] h-2 rounded-full"></div>
                 </div>
             </div>
 
             <div className="node hover:scale-105 duration-200 cursor-pointer">
-            <span className="flex py-2 gap-2 text-sm font-medium"><img src="/public/node-icon.png" alt="" className="w-[24px]" />NODE JS</span>
+            <span className="flex py-2 gap-2 text-sm font-medium"><img src={nodeIcon} alt="" className="w-[24px]" />NODE JS</span>
             <div className="w-[80%] h-2 bg-slate-200 rounded-full">
                     <div className="bg-[#83bdf5] w-[68%] h-2 rounded-full"></div>
                 </div>
             </div>
 
             <div className="express hover:scale-105 duration-200 cursor-pointer">
-            <span className="flex py-2 gap-2 text-sm font-medium"><img src="/public/express-icon.png" alt="" className="w-[24px]" />EXPRESS JS</span>
+            <span className="flex py-2 gap-2 text-sm font-medium"><img src={expressIcon} alt="" className="w-[24px]" />EXPRESS JS</span>
             <div className="w-[80%] h-2 bg-slate-200 rounded-full">
                     <div className="bg-[#83bdf5] w-[55%] h-2 rounded-full"></div>
                 </div>
             </div>
 
             <div className="mongodb hover:scale-105 duration-200 cursor-pointer">
-            <span className="flex py-2 gap-2 text-sm font-medium"><img src="/public/mongo-icon.jpg" alt="" className="w-[24px]" />MONGO DB</span>
+            <span className="flex py-2 gap-2 text-sm font-medium"><img src={mongodbIcon} alt="" className="w-[24px]" />MONGO DB</span>
             <div className="w-[80%] h-2 bg-slate-200 rounded-full">
                     <div className="bg-[#83bdf5] w-[50%] h-2 rounded-full"></div>
                 </div>
             </div>
 
             <div className="git hover:scale-105 duration-200 cursor-pointer">
-            <span className="flex py-2 gap-2 text-sm font-medium"><img src="/public/github-icon.png" alt="" className="w-[24px]" />GIT & GITHUB</span>
+            <span className="flex py-2 gap-2 text-sm font-medium"><img src={githubIcon} alt="" className="w-[24px]" />GIT & GITHUB</span>
             <div className="w-[80%] h-2 bg-slate-200 rounded-full">
                     <div className="bg-[#83bdf5] w-[60%] h-2 rounded-full"></div>
                 </div>
