c41cad81f0d5f5d5932347dd5454dd16eb388740
SumanSaha <8759127658itisaha@gmail.com>
2024-08-05 22:20:14 +0530

    fixed the footer padding

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,14 +7,14 @@ import twitterIcon from '../../public/twitter.png'
 
 const Contact = () => {
   return (
-    <section id='contact' className='w-full min-h-[30vh] md:min-h-[60vh] bg-[#232d39] flex flex-col items-center justify-center'>
+    <section id='contact' className='w-full min-h-[40vh] md:min-h-[60vh] bg-[#232d39] flex flex-col items-center justify-center'>
         <div className="w-[80%] flex items-center justify-center flex-col gap-3">
             <spn className="text-gray-300 font-semibold">CONTACT ME</spn>
             <h1 className="text-white text-2xl md:text-5xl font-medium text-center">Get A Project? Let's Talk!</h1>
         </div>
 
         <footer className='relative top-[1rem] md:top-[9rem] m-4 p-2 w-[80%] h-12 flex flex-col md:flex-row items-center justify-between border-t-2 '>
-        <div className="icons flex gap-4 invert-[40%]">
+        <div className="icons flex gap-8 invert-[40%] p-6">
                 <a href="https://www.facebook.com/profile.php?id=100015689491741" className=""><img src={facebookIcon} alt="" className="" /></a>
                 <a href="https://www.instagram.com/sum4.an/" className=""><img src={instagramIcon} alt="" className="" /></a>
                 <a href="https://www.linkedin.com/in/suman-saha-225958291/" className=""><img src={linkedinIcon} alt="" className="" /></a>
@@ -22,7 +22,7 @@ const Contact = () => {
                 <a href="https://x.com/Dev_Suman07" className=""><img src={twitterIcon} alt="" className="" /></a>
               </div>
 
-              <div className="copyright invert-[80%] text-nowrap p-2">
+              <div className="copyright invert-[80%] text-nowrap p-">
               <p>&copy; 2024 Suman Saha. All rights reserved.</p>
               </div>
         </footer>
