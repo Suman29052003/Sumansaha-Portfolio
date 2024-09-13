c2c63e95c6d473b98e5cdb2b21a68f856924eed6
SumanSaha <8759127658itisaha@gmail.com>
2024-09-13 19:02:07 +0530

    "Added @vercel/analytics dependency and imported Analytics component in App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import About from './components/About'
-import Skills from './components/Skills'
-import Service from './components/Service'
-import Education from './components/Education'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Service from "./components/Service";
+import Education from "./components/Education";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
     <>
-    <Navbar/>
-     <Home/>
-     <About/>
-     <Skills/>
-     <Service/>
-     <Education/>
-     <Projects/>
-     <Contact/>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Service />
+      <Education />
+      <Projects />
+      <Contact />
+      <Analytics />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
